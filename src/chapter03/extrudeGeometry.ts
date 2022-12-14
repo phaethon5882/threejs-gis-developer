435e8fe666439af63a9588d7e081326064b432da
benny.back(백윤기)/kakao <benny.back@kakaocorp.com>
2022-12-14 22:36:25 +0900

    chapter03-Geometry03-fontLoader

--- src/chapter03/extrudeGeometry.ts
+++ src/chapter03/extrudeGeometry.ts
@@ -1,16 +1,16 @@
 import * as THREE from 'three';
-import { Object3D, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { Group, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-type ModelName = 'solarSystem' | 'earthOrbit' | 'moonOrbit';
-type ModelMap = Record<ModelName, Object3D>;
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import fontJson from 'three/examples/fonts/helvetiker_regular.typeface.json';
 
 class App {
   private readonly container: HTMLDivElement;
   private readonly renderer: WebGLRenderer;
   private readonly scene: Scene;
   private readonly camera: PerspectiveCamera;
-  private readonly models: ModelMap;
+  private readonly model: Group;
 
   constructor(container: HTMLDivElement) {
     this.container = container;
@@ -22,7 +22,7 @@ class App {
     this.container.appendChild(this.renderer.domElement);
 
     this.setupLight();
-    this.models = this.setupModels();
+    this.model = this.setupModel();
 
     this.setupControls();
 
@@ -40,7 +40,7 @@ class App {
     const far = 100;
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 25);
+    camera.position.set(0, 0, 20);
 
     return camera;
   };
@@ -53,54 +53,32 @@ class App {
     this.scene.add(light);
   };
 
-  private readonly setupModels = (): ModelMap => {
-    const solarSystem = new THREE.Object3D();
-    this.scene.add(solarSystem);
-
-    // 공용 구 geometry
-    const sphereGeometry = new THREE.SphereGeometry(1, 12, 12);
-
-    // 태양 추가
-    const sunMaterial = new THREE.MeshPhongMaterial({
-      emissive: 0xffff00,
-      flatShading: true,
+  private readonly setupModel = (): Group => {
+    const fontLoader = new FontLoader();
+    const textGeometry = new TextGeometry('Yunki', {
+      font: fontLoader.parse(fontJson),
+      size: 5,
+      height: 1.5,
+      curveSegments: 4,
+      bevelEnabled: true,
+      bevelThickness: 0.7,
+      bevelSize: 0.7,
+      bevelSegments: 2,
     });
-    const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-    sunMesh.scale.set(3, 3, 3);
-    solarSystem.add(sunMesh);
 
-    // 지구 추가
-    const earthMaterial = new THREE.MeshPhongMaterial({
-      color: 0x2233ff,
-      emissive: 0x112244,
-      flatShading: true,
-    });
-    const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-    const earthOrbit = new THREE.Object3D();
-    earthOrbit.position.set(10, 0, 0);
-    earthOrbit.add(earthMesh);
-    solarSystem.add(earthOrbit);
+    const textMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+    const text = new THREE.Mesh(textGeometry, textMaterial);
 
-    // 달 추가
-    const moonMaterial = new THREE.MeshPhongMaterial({
-      color: 0x888888,
-      emissive: 0x222222,
-      flatShading: true,
-    });
-    const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-    moonMesh.scale.set(0.5, 0.5, 0.5);
+    const wireframeGeometry = new THREE.WireframeGeometry(textGeometry);
+    const wireframeMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    const wireframe = new THREE.LineSegments(wireframeGeometry, wireframeMaterial);
 
-    // 지구 관점에서 x 축으로 2만큼 떨어지게 배치
-    const moonOrbit = new THREE.Object3D();
-    moonOrbit.add(moonMesh);
-    moonOrbit.position.set(2, 0, 0);
-    earthOrbit.add(moonOrbit);
+    const group = new THREE.Group();
+    group.add(text);
+    group.add(wireframe);
+    this.scene.add(group);
 
-    return {
-      solarSystem,
-      earthOrbit,
-      moonOrbit,
-    };
+    return group;
   };
 
   private readonly setupControls = (): void => {
@@ -122,11 +100,8 @@ class App {
   };
 
   private readonly update = (time: DOMHighResTimeStamp): void => {
-    const seconds = time * 0.001;
-    const { solarSystem, earthOrbit, moonOrbit } = this.models;
-    solarSystem.rotation.set(0, seconds * 0.5, 0);
-    earthOrbit.rotation.set(0, seconds * 2, 0);
-    moonOrbit.rotation.set(0, seconds * 0.005, 0);
+    // this.model.rotation.x = time * 0.001;
+    // this.model.rotation.y = time * 0.001;
   };
 }
 
